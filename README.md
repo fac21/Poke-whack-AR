360a6049307c4c45fb7b440b4da0166269f9f6b4
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-04-09 14:55:11 +0100

    functionality

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Poke-whack-AR
+
+We watched Wes Bos's Whack-A-Mole [video](https://www.youtube.com/watch?v=toNFfAaWghU&list=PLu8EoSxDXHP6CGK4YVJhL_VWetA865GOH&index=30) and recreated the functions from memory, to create our own version of the game.
+
+We used a Pokemon API to get the mole image. 
+
+## Issues
+
+We added an alert which then kept repeating, so in order to stop this, we added a clearInterval() to the function.
+
+We had too many moles appearing at one time, giving the appearance of it speeding it up. We added a factor to the if statement that limited to number of moles popped up to less than 4.
+
+![](eg-2.png)
--- app.js
+++ app.js
@@ -1,50 +1,75 @@
-const moles = Array.from(document.querySelectorAll(".mole"));
+const moles = document.querySelectorAll(".mole");
 const holes = document.querySelectorAll(".hole");
+const score = document.querySelector(".score")
 let lastHole;
 let count = 0;
+let timeLapsed = false;
 
-let name = "bulbasaur"
- let pokepic = () => {
-     fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
-        .then((response) => response.json())
-        .then((pokemonData) => {
-            console.dir(pokemonData);
-            console.log(moles)
-            moles.forEach(mole => mole.src = pokemonData.sprites.front_default);
-          })
-    }
+let name = "bulbasaur";
+let pokepic = () => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then((response) => response.json())
+    .then((pokemonData) => {
+      console.dir(pokemonData);
+      console.log(moles);
+      moles.forEach((mole) => (mole.src = pokemonData.sprites.front_default));
+    });
+};
 
 pokepic();
 
-
-function randomHole(){
-    let index = Math.floor(Math.random()*holes.length);
-    let hole = holes[index];
-    if (hole === lastHole) {return randomHole()}
-    lastHole = hole
-    return hole;
+function randomHole() {
+  let index = Math.floor(Math.random() * holes.length);
+  let hole = holes[index];
+  if (hole === lastHole) {
+    return randomHole();
+  }
+  lastHole = hole;
+  return hole;
 }
 
-function randomTime(min, max){
-    let int = max-min;
-    return Math.floor(Math.random()*int + min);
+function randomTime(min, max) {
+  let int = max - min;
+  return Math.floor(Math.random() * int + min);
 }
 
-function peep (){
-    let time = randomTime(1000, 5000);
-    let hole = randomHole();
-    let mole = hole.querySelector(".mole");
-    mole.classList.add("up");
-    setInterval(() => {mole.remove("up")}, time);
-    return mole
+function peep() {
+  let time = randomTime(1000, 5000);
+  let hole = randomHole();
+  let mole = hole.querySelector(".mole");
+  console.log(hole, mole);
+
+  mole.classList.add("up");
+  setInterval(() => {
+    mole.classList.remove("up");
+    if (!timeLapsed && document.querySelectorAll(".up").length < 4) {
+      peep();
+    }
+  }, time);
+  //return mole;
 }
 
-function startGame(){
-    count = 0;
+function startGame() {
+  timeLapsed = false;
+  count = 0;
+  peep();
 
-    peep()
+  let intStart = setInterval(() => {
+    timeLapsed = true;
+    alert(`Game over! You scored ${count}!`);
+    clearInterval(intStart);
+  }, 10000);
 }
 
-function bonk (){
+document.querySelector("button").addEventListener("click", startGame);
 
+
+function bonk(e) {
+if (!e.isTrusted) return;
+count ++;
+this.classList.remove("up");
+score.textContent = count;
+console.log(count)
 }
+
+moles.forEach((mole) => mole.addEventListener("click", bonk))
